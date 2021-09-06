64a8c0c54ed0049c73a1200f9d86696952a2f2a0
tddebart <tddebart@gmail.com>
2021-09-06 17:06:32 +0200

    new sloth effect

--- BossSlothsCards/TempEffects/SlothEffect.cs
+++ BossSlothsCards/TempEffects/SlothEffect.cs
@@ -1,4 +1,6 @@
-﻿using ModdingUtils.MonoBehaviours;
+﻿using System;
+using BossSlothsCards.Utils;
+using ModdingUtils.MonoBehaviours;
 using UnityEngine;
 
 namespace BossSlothsCards.TempEffects
@@ -8,11 +10,16 @@ namespace BossSlothsCards.TempEffects
         public float multiplier;
         private const float timeToMax = 25;
         public float maxMultiplier = 3;
+
+        public GameObject circle;
+        public SpriteRenderer renderer;
+
+        private float timeSinceAttack;
         
         public override CounterStatus UpdateCounter()
         {
-            float timeSince = gun.sinceAttack;
-            multiplier = Mathf.Clamp(((maxMultiplier - 1f) / (timeToMax)) * timeSince + 1f, 1f, maxMultiplier);
+            timeSinceAttack = gun.sinceAttack;
+            multiplier = Mathf.Clamp(((maxMultiplier - 1f) / (timeToMax)) * timeSinceAttack + 1f, 1f, maxMultiplier);
             
             return CounterStatus.Apply;
         }
@@ -25,6 +32,36 @@ namespace BossSlothsCards.TempEffects
         public override void OnApply()
         {
             
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            circle = Instantiate(BossSlothCards.EffectAsset.LoadAsset<GameObject>("Orange circle"), transform.Find("Particles"));
+            circle.name = "SlothCircle";
+            Destroy(circle.GetComponent<Animator>());
+            renderer = circle.GetComponent<SpriteRenderer>();
+            circle.transform.localScale = new Vector3(0.38f, 0.38f, 0.38f);
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (multiplier > 1)
+            {
+                circle.SetActive(true);
+            }
+            else
+            {
+                circle.SetActive(false);
+            }
+
+            var rendererColor = renderer.color;
+            rendererColor.a = Mathf.InverseLerp(0, 25, timeSinceAttack);
+            renderer.color = rendererColor;
+
+            renderer.color = Color.Lerp(new Color32(98, 37, 14, 255), new Color32(255, 255, 10, 255),
+                Mathf.InverseLerp(0, 25, timeSinceAttack));
         }
 
         public override void Reset()
